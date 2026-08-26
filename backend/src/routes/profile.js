9d303570a26217ee1a776cea1f30ea0cd5e4863e
nikhilrana453 <rananikhil453@gmail.com>
2026-08-26 23:43:35 +1200

    Use bcryptjs in profile.js

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const prisma  = require('../prismaClient');
-const bcrypt  = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { authenticateToken } = require('../middleware/auth');
 
 // GET /api/profile
